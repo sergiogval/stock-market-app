b2ba713fb9896924cb63855a9a9eef526ba0bf51
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-12-24 17:18:58 +0500

    Add responsive searchbar to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({
+  handleSearch, handelEvent, active, nonActive, handelCloseEvent,
+}) => {
   const [hour, setHour] = useState('');
   const [minutes, setMinutes] = useState('');
 
@@ -27,14 +30,28 @@ const Header = () => {
   return (
     <div className="main-header">
       <div className="main-inner-header">
-        <p>
-          {hour}
-          {' '}
-          :
-          {' '}
-          {minutes}
-        </p>
-        <p>stock app</p>
+        <div className={`inner-header-default ${nonActive}`}>
+          <p>
+            {hour}
+            {' '}
+            :
+            {' '}
+            {minutes}
+          </p>
+          <p>stock app</p>
+          <button type="button" className="searchBtn" onClick={handelEvent}> </button>
+        </div>
+        <div className={`main-search-bar ${active}`}>
+          <input
+            type="search"
+            name="main-search-bar"
+            onChange={handleSearch}
+            onKeyUp={handleSearch}
+            className={`main-search-bar-int ${active}`}
+            placeholder="Search by stock name or company name.."
+          />
+          <button type="button" className="hideSearchBtn" onClick={handelCloseEvent}>x</button>
+        </div>
       </div>
       <h1>Stock Market</h1>
       <div className="main-sub-header">
@@ -43,6 +60,14 @@ const Header = () => {
       </div>
     </div>
   );
+};
+
+Header.propTypes = {
+  active: PropTypes.element.isRequired,
+  nonActive: PropTypes.element.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  handelEvent: PropTypes.func.isRequired,
+  handelCloseEvent: PropTypes.func.isRequired,
 };
 
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './Nav';
 import Header from './Header';
 import Footer from './Footer';
 import '../assets/stylesheets/home.css';
 
-const Home = () => (
-  <div className="main-container">
-    <Header />
-    <Nav />
-    <Footer />
-  </div>
-);
+const Home = () => {
+  const [searchOn, setSearchOn] = useState(false);
+
+  const openSearchBar = () => setSearchOn(true);
+  const closeSearchBar = () => setSearchOn(false);
+
+  return (
+    <div className="main-container">
+      <Header
+        handelEvent={openSearchBar}
+        handelCloseEvent={closeSearchBar}
+        active={
+          searchOn
+            ? 'search-is-on'
+            : ''
+        }
+        nonActive={
+          searchOn
+            ? 'inner-header-search'
+            : ''
+        }
+      />
+      <Nav />
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
